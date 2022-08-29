2e5d1151f061738158aeb8095b6509f1b59be02f
Vanessa Barone <vanbarone.milani@gmail.com>
2022-08-29 12:03:00 -0300

    Criação da classe Conexao

---
+++ Utils/Conexao.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+
+namespace ClinicaVeterinaria.Utils
+{
+    public static class Conexao
+    {
+
+        private static string connectionString = "Data Source=DESKTOP-690C72D\\SQLEXPRESS;Initial Catalog=ClinicaVeterinaria;User ID=sa; Password=Sql123@;encrypt=false";
+
+        public static SqlConnection GetConection()
+        {
+            SqlConnection conexao = new SqlConnection(connectionString);
+
+            return conexao;
+        }
+
+
+    }
+}
